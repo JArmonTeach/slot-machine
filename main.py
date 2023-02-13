c33377418a236bb1db8c4bbfabb2bde2a7cb7c68
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-02-13 17:10:45 -0500

    feat: complete deposit function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #slot machine program
+
+def deposit():
+    while True:
+        amount = input("Enter amount to be deposited: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount >= 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    
+    return amount
