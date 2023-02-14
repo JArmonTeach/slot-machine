aa6e372a460f26fbb7a651a143bd6a8c80ad19e0
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-02-13 20:09:40 -0500

    feat: add while loop in main function

--- main.py
+++ main.py
@@ -49,8 +49,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
 
+        if total_bet > balance:
+            print("You do not have enough funds for that bet, your current balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
     
 
 
